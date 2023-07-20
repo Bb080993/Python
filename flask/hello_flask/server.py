5bc3c57b6946543863abc85c8bdd573948495a42
Brittany Francis <brittanyfrancis200@gmail.com>
2023-07-20 11:44:39 -0400

    added html

--- flask/hello_flask/server.py
+++ flask/hello_flask/server.py
@@ -7,5 +7,5 @@ app = Flask(__name__)    # Create a new instance of the Flask class called "app"
 def hello_world():
     return render_template("index.html")  # Return the string 'Hello World!' as a response
 if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
-    app.run(debug=True)    # Run the app in debug mode.
+    app.run(debug=True, port=8000)    # Run the app in debug mode.
 
